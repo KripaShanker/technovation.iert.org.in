3c028edbbdc5dd4d3c90a32d038dda1823b46bb8
kripashanker <iamkripashanker@gmail.com>
2016-04-04 19:41:29 +0530

    mirth images

--- arena/connectdb.php
+++ arena/connectdb.php
@@ -1,5 +1,7 @@
 <?php
 error_reporting(E_ALL ^ E_DEPRECATED);
-$con=mysql_connect("localhost","kripa","iert@123");
-$db=mysql_select_db("event",$con);
+
+
+mysql_connect('localhost','root','');
+mysql_select_db("iert");
 ?>
