f7dc42691108bd98f7b56b8024e01d93165eddb7
kripashanker <iamkripashanker@gmail.com>
2016-04-04 21:04:19 +0530

    mirth images

--- arena/events/event.php
+++ arena/events/event.php
@@ -232,7 +232,7 @@ Harbour.Space on our Twitter and Facebook.</p>
 			 {
 				echo "
 						<div href='#' class='peoples__item peoples__item_count_7'>
-				<figure class='peoples__item__photo'><img src=coordinator_image/$rows[lead_image] alt='' /></figure>
+				<figure class='peoples__item__photo'><img src=coordinator_image/default.png alt='' /></figure>
 				<strong class='peoples__item__name'>
 					<span class='peoples__item__name__first'>
 					
