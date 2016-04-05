402104db46512e81e5350bdee648d6689da0ea01
kripashanker <iamkripashanker@gmail.com>
2016-04-05 18:49:25 +0530

    image

--- coordinators.php
+++ coordinators.php
@@ -2,14 +2,14 @@
 function details($img,$name,$mob)
 {
 
-if($img==null)
-{
-	$img="default.png";
-}
+	if($img==null)
+	{
+		$img="default.png";
+	}
 
-echo "<div class='span4  jeg_do_animate'  data-animation='janimate-fadein'   data-transform='janimate-scale'   data-position='janimpos-bottom' ><img class='aligncenter size-full wp-image-90' width='100' src='coordinator_image/$img' /></p>
+	echo "<div class='span4  jeg_do_animate'  data-animation='janimate-fadein'   data-transform='janimate-scale'   data-position='janimpos-bottom' ><img style='border-radius:50%' class='aligncenter size-full wp-image-90' width='150' src='coordinator_image/$img' /></p>
 	<p style='text-align: center;'><strong>$name</strong><br>$mob</p>
-	</div>";
+</div>";
 }
 ?>
 
@@ -22,156 +22,214 @@ include_once 'hd.php';
 	<div class="contentheaderspace"></div>
 
 
-<section 
-		class="doubletopmargin doublebottommargin  light   " 
-		data-id="page-blog-layout" 
-		data-title="Blog & Page Layout"
-		 
-		style="background-image : url(wp-content/uploads/2014/05/pagelayout_bg.png);background-position : center center;background-repeat	: repeat;">
-		
-				
-				<div class='parallaxoverlay' style='background:rgba(255,255,255,0);'></div>		
+	<section 
+	class="doubletopmargin doublebottommargin  light   " 
+	data-id="page-blog-layout" 
+	data-title="Blog & Page Layout"
+
+	style="background-image : url(wp-content/uploads/2014/05/pagelayout_bg.png);background-position : center center;background-repeat	: repeat;">
+
+
+	<div class='parallaxoverlay' style='background:rgba(255,255,255,0);'></div>		
+
+
+
+	<div class="sectioncontainer">		
+		<div class='section-header  position-center'>
+			<p id="sponsors"></p>
+			<h1>Head Coordinator</h1>
+			<span class='sectionline'></span>
+			<em>The Big Boss</em>
+		</div> 
+
+		<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+			<?php details('mohit.jpg','Mohit Singh','');?>
 			
-		
+		</div>
+
 		<div class="sectioncontainer">		
 			<div class='section-header  position-center'>
-			<p id="sponsors"></p>
-		<h1>Scheduling Team</h1>
-		<span class='sectionline'></span>
-		<em>Space-Time Management!</em>
-	</div> 
-	
-	<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
-	<?php details('default.png','Dhananjay','8948321455');?>
-	<?php details('default.png','Abhishek','9457605352');?>
-	<?php details('default.png','Himmayat','7052601459');?>
-	</div>
-	<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' >
-	<?php details('default.png','Shashwat','7408165484');?>
-	<?php details('default.png','Paritosh','9793861928');?>
-	<?php details('default.png','Kripashanker','8896608650');?>
-	</div>
-	<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' >
-	<?php details('default.png','Shruti Sahu',' ');?>
-	<?php details('default.png','Divyam','8605180061');?>
-	<?php details('default.png','Nitish',' ');?>
-	</div><div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' >
-	<?php details('default.png','Sachin Pandey',' ');?>
-	</div>
+				<p id="sponsors"></p>
+				<h1>Chief Coordinators</h1>
+				<span class='sectionline'></span>
+				<em>The warriors of administration</em>
+			</div> 
 
-	<div class="sectioncontainer">		
-			<div class='section-header  position-center'>
-			<p id="sponsors"></p>
-		<h1>Registration Team - College</h1>
-		<span class='sectionline'></span>
-		<em>Trackers of local participation</em>
-	</div> 
-	
-	<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
-	<?php details('default.png','Reyansh (EL)','9005472398');?>
-	<?php details('default.png','Shubham Desai (ME)','8874511497');?>
-	<?php details('default.png','Shashwat (EL)','7408165484');?>
-	</div>
-	<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
-	<?php details('default.png','Ravishankar (CS)','8896101102');?>
-	</div>
-	
-	<div class="sectioncontainer">		
-			<div class='section-header  position-center'>
-			<p id="sponsors"></p>
-		<h1>Registration Team - Promotion</h1>
-		<span class='sectionline'></span>
-		<em>Trackers of foreign participation</em>
-	</div> 
-	
-	<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
-	<?php details('default.png','Rajat (CE)','8543044314');?>
-	<?php details('default.png','Shanatanu (EL)','8563019639');?>
-	</div>
-		
-	<div class="sectioncontainer">		
-			<div class='section-header  position-center'>
-			<p id="sponsors"></p>
-		<h1>Web Team</h1>
-		<span class='sectionline'></span>
-		<em>Connecting us all!</em>
-	</div> 
-	
-	<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
-	<?php details('default.png','Ajay Pandey','9125580401');?>
-	<?php details('default.png','Kripashanker','8896608650');?>
-	<?php details('default.png','Prakhar Srivastava','9369168442');?>
-	</div>
-	<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
-	<?php details('default.png','Ravi Singh Kushwaha','8303210846');?>
-	</div>	
-		
-	<div class="sectioncontainer">		
-			<div class='section-header  position-center'>
-			<p id="sponsors"></p>
-		<h1>Sponsors Team</h1>
-		<span class='sectionline'></span>
-		<em>The awesome advertisers..</em>
-	</div> 
-	
-	<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
-	<?php details('default.png','Abhishek (CE)','9457605352');?>
-	<?php details('default.png','Rajat (CE)','8543044314');?>
-	<?php details('default.png','Gyandeep (CS)','9889389190');?>
-	</div>
-	<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
-	<?php details('default.png','Sujeet (IP)','9506715996');?>
-	<?php details('default.png','Pragya Kaushik (ME)',' ');?>
-	</div>
-	
-	<div class="sectioncontainer">		
-			<div class='section-header  position-center'>
-			<p id="sponsors"></p>
-		<h1>Poster Printing &amp; Design Team</h1>
-		<span class='sectionline'></span>
-		<em>The presenters of Technovation on canvas..</em>
-	</div> 
-	
-	<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
-	<?php details('default.png','Pragyat (IC)','9376519544');?>
-	<?php details('default.png','Shubham Singh (CS)','8726609870');?>
-	<?php details('default.png','Atif (ME)','8181036038');?>
-	</div>
-	<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
-	<?php details('default.png','Devendra (ME)','9795697646');?>
-	</div>
-	
-	<div class="sectioncontainer">		
-			<div class='section-header  position-center'>
-			<p id="sponsors"></p>
-		<h1>Promotion Team</h1>
-		<span class='sectionline'></span>
-		<em>Look out for us; We will persuade you to participate</em>
-	</div> 
-	
-	<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
-	<?php details('default.png','Atif (ME)','8181036038');?>
-	<?php details('default.png','Meenakshi (CS)',' ');?>
-	<?php details('default.png','Vibhor (EL)','8896328804');?>
-	</div>
-	<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
-	<?php details('default.png','Shantanu (EL)','8563019639');?>
-	<?php details('default.png','Shashwat (EL)','7408165484');?>
-	<?php details('default.png','Vaishali (ME)',' ');?>
-	</div>
-	<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
-	<?php details('default.png','Shubhi (ME)',' ');?>
-	<?php details('default.png','Rajat (CE)','8543044314');?>
-	<?php details('default.png','Kalpana (EL)',' ');?>
-	</div>
-	<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
-	<?php details('default.png','Harshit Gupta (CS)','8765630745');?>
-	</div>
-	
-	</section>
-	
-	</div>
 
-<?php
-include_once 'ft.php';
-?>
+
+
+
+
+			<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+				<?php details('gyandeep.jpg','Gyandeep Mishra','9889389190');?>
+				<?php details('default.png','Sujeet Chand','9506715996');?>
+				<?php details('default.png','Devendra Kr Gond','');?>
+				
+
+			</div>
+			<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+				<?php details('default.png','Harsh Singh','');?>
+				<?php details('default.png','Vivek','');?>
+				<?php details('default.png','Rajat','');?>
+				
+			</div>
+			<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+				<?php details('default.png','Atif Mohd','');?>
+				<?php details('default.png','Prapunj Tiwari','');?>
+				<?php details('default.png','Shubham Desai','');?>
+				
+			</div>
+			<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+
+				<?php details('default.png','Pragya Kaushik','');?>
+
+			</div>
+		</div>
+
+		<div class="sectioncontainer">		
+						<div class='section-header  position-center'>
+							<p id="sponsors"></p>
+							<h1>Web Team</h1>
+							<span class='sectionline'></span>
+							<em>Connecting us all!</em>
+						</div> 
+
+						<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+							<?php details('ajay.jpg','Ajay Pandey','9125580401');?>
+							<?php details('kripa.jpg','Kripashanker','8896608650');?>
+							<?php details('prakhar.jpg','Prakhar Srivastava','9369168442');?>
+						</div>
+						<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+							<?php details('ravi.jpg','Ravi Singh Kushwaha','8303210846');?>
+						</div>	
+
+		<div class="sectioncontainer">		
+				<div class='section-header  position-center'>
+					<p id="sponsors"></p>
+					<h1>Registration Team - College</h1>
+					<span class='sectionline'></span>
+					<em>Trackers of local participation</em>
+				</div> 
+
+				<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+					<?php details('ravishankar.jpg','Ravishankar (CS)','8896101102');?>
+					
+					<?php details('default.png','Shubham Desai (ME)','8874511497');?>
+					<?php details('default.png','Shashwat (EL)','7408165484');?>
+				</div>
+				<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+					<?php details('default.png','Reyansh (EL)','9005472398');?>
+				</div>
+
+				<div class="sectioncontainer">		
+					<div class='section-header  position-center'>
+						<p id="sponsors"></p>
+						<h1>Registration Team - Promotion</h1>
+						<span class='sectionline'></span>
+						<em>Trackers of foreign participation</em>
+					</div> 
+
+					<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+						<?php details('default.png','Rajat (CE)','8543044314');?>
+						<?php details('default.png','Shanatanu (EL)','8563019639');?>
+					</div>
+
+
+			<div class="sectioncontainer">		
+				<div class='section-header  position-center'>
+					<p id="sponsors"></p>
+					<h1>Scheduling Team</h1>
+					<span class='sectionline'></span>
+					<em>Space-Time Management!</em>
+				</div> 
+
+				<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+					<?php details('default.png','Dhananjay','8948321455');?>
+					<?php details('default.png','Abhishek','9457605352');?>
+					<?php details('default.png','Himmayat','7052601459');?>
+				</div>
+				<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' >
+					<?php details('default.png','Shashwat','7408165484');?>
+					<?php details('paritosh.jpg','Paritosh','9793861928');?>
+					<?php details('kripa.jpg','Kripashanker','8896608650');?>
+				</div>
+				<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' >
+					<?php details('default.png','Shruti Sahu',' ');?>
+					<?php details('default.png','Divyam','8605180061');?>
+					<?php details('default.png','Nitish',' ');?>
+				</div><div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' >
+				<?php details('default.png','Sachin Pandey',' ');?>
+			</div>
+
+			
+					
+
+						<div class="sectioncontainer">		
+							<div class='section-header  position-center'>
+								<p id="sponsors"></p>
+								<h1>Sponsors Team</h1>
+								<span class='sectionline'></span>
+								<em>The awesome advertisers..</em>
+							</div> 
+
+							<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+								<?php details('default.png','Abhishek (CE)','9457605352');?>
+								<?php details('default.png','Rajat (CE)','8543044314');?>
+								<?php details('gyandeep.jpg','Gyandeep (CS)','9889389190');?>
+							</div>
+							<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+								<?php details('default.png','Sujeet (IP)','9506715996');?>
+								<?php details('default.png','Pragya Kaushik (ME)',' ');?>
+							</div>
+
+							<div class="sectioncontainer">		
+								<div class='section-header  position-center'>
+									<p id="sponsors"></p>
+									<h1>Poster Printing &amp; Design Team</h1>
+									<span class='sectionline'></span>
+									<em>The presenters of Technovation on canvas..</em>
+								</div> 
+
+								<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+									<?php details('default.png','Pragyat (IC)','9376519544');?>
+									<?php details('shubham.jpg','Shubham Singh (CS)','8726609870');?>
+									<?php details('default.png','Atif (ME)','8181036038');?>
+								</div>
+								<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+									<?php details('default.png','Devendra (ME)','9795697646');?>
+								</div>
+
+								<div class="sectioncontainer">		
+									<div class='section-header  position-center'>
+										<p id="sponsors"></p>
+										<h1>Promotion Team</h1>
+										<span class='sectionline'></span>
+										<em>Look out for us; We will persuade you to participate</em>
+									</div> 
+
+									<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+										<?php details('default.png','Atif (ME)','8181036038');?>
+										<?php details('meenakshi.jpg','Meenakshi (CS)',' ');?>
+										<?php details('default.png','Vibhor (EL)','8896328804');?>
+									</div>
+									<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+										<?php details('default.png','Shantanu (EL)','8563019639');?>
+										<?php details('default.png','Shashwat (EL)','7408165484');?>
+										<?php details('default.png','Vaishali (ME)',' ');?>
+									</div>
+									<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+										<?php details('default.png','Shubhi (ME)',' ');?>
+										<?php details('default.png','Rajat (CE)','8543044314');?>
+										<?php details('default.png','Kalpana (EL)',' ');?>
+									</div>
+									<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+										<?php details('harshit.jpg','Harshit Gupta (CS)','8765630745');?>
+									</div>
+
+								</section>
+
+							</div>
+
+							<?php
+							include_once 'ft.php';
+							?>
--- techteam.php
+++ techteam.php
@@ -2,14 +2,14 @@
 function details($img,$name,$mob)
 {
 
-if($img==null)
-{
-	$img="default.png";
-}
+	if($img==null)
+	{
+		$img="default.png";
+	}
 
-echo "<div class='span4  jeg_do_animate'  data-animation='janimate-fadein'   data-transform='janimate-scale'   data-position='janimpos-bottom' ><img class='aligncenter size-full wp-image-90' width='100' src='coordinator_image/$img' /></p>
+	echo "<div class='span4  jeg_do_animate'  data-animation='janimate-fadein'   data-transform='janimate-scale'   data-position='janimpos-bottom' ><img style='border-radius:50%' class='aligncenter size-full wp-image-90' width='150' src='coordinator_image/$img' /></p>
 	<p style='text-align: center;'><strong>$name</strong><br>$mob</p>
-	</div>";
+</div>";
 }
 ?>
 
@@ -22,48 +22,48 @@ include_once 'hd.php';
 	<div class="contentheaderspace"></div>
 
 
-<section 
-		class="doubletopmargin doublebottommargin  light   " 
-		data-id="page-blog-layout" 
-		data-title="Blog & Page Layout"
-		 
-		style="background-image : url(wp-content/uploads/2014/05/pagelayout_bg.png);background-position : center center;background-repeat	: repeat;">
-		
-				
-				<div class='parallaxoverlay' style='background:rgba(255,255,255,0);'></div>		
-			
-		
-		<div class="sectioncontainer">		
-			<div class='section-header  position-center'>
+	<section 
+	class="doubletopmargin doublebottommargin  light   " 
+	data-id="page-blog-layout" 
+	data-title="Blog & Page Layout"
+
+	style="background-image : url(wp-content/uploads/2014/05/pagelayout_bg.png);background-position : center center;background-repeat	: repeat;">
+
+
+	<div class='parallaxoverlay' style='background:rgba(255,255,255,0);'></div>		
+
+
+	<div class="sectioncontainer">		
+		<div class='section-header  position-center'>
 			<p id="sponsors"></p>
-		<h1>Technical Team</h1>
-		<span class='sectionline'></span>
-		<em>We know our stuff!</em>
-	</div> 
-	
-	<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
-	<?php details('default.png','Aadil Srivastava',' ');?>
-	<?php details('default.png','Ajay Pandey',' ');?>
-	<?php details('default.png','Amit K. Yadav',' ');?>
-	</div>
-	<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' >
-	<?php details('default.png','Kripashanker Tiwari',' ');?>
-	<?php details('default.png','Mayank Kumar',' ');?>
-	<?php details('default.png','Prakhar Srivastava',' ');?>
-	</div>
-	<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' >
-	<?php details('default.png','Rakesh',' ');?>
-	<?php details('default.png','Ravi Singh Kushwaha',' ');?>
-	<?php details('default.png','Ravishanker Chaubey',' ');?>
-	</div><div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' >
-	<?php details('default.png','Sanjay Patel',' ');?>
-	<?php details('default.png','Shubham Singh',' ');?>
+			<h1>Technical Team</h1>
+			<span class='sectionline'></span>
+			<em>We know our stuff!</em>
+		</div> 
+
+		<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' > 
+			<?php details('aadil.jpg','Aadil Srivastava',' ');?>
+			<?php details('ajay.jpg','Ajay Pandey',' ');?>
+			<?php details('amit.jpg','Amit K. Yadav',' ');?>
+		</div>
+		<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' >
+			<?php details('kripa.jpg','Kripashanker Tiwari',' ');?>
+			<?php details('mayank.jpg','Mayank Kumar',' ');?>
+			<?php details('prakhar.jpg','Prakhar Srivastava',' ');?>
+		</div>
+		<div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' >
+			<?php details('rakesh.jpg','Rakesh',' ');?>
+			<?php details('ravi.jpg','Ravi Singh Kushwaha',' ');?>
+			<?php details('ravishankar.jpg','Ravishanker Chaubey',' ');?>
+		</div><div class='row-fluid   jeg_animate_sequence jeg_animate_fast fast '  data-speed='150' data-offset='90' >
+		<?php details('sanjay.jpg','Sanjay Patel',' ');?>
+		<?php details('shubham.jpg','Shubham Singh',' ');?>
 	</div>
 
 	
-	</section>
-	
-	</div>
+</section>
+
+</div>
 
 <?php
 include_once 'ft.php';
